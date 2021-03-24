360a02a69d952cc3b450691a9d0d8aef273f9696
amanpreet93 <70941064+amanpreet93@users.noreply.github.com>
2021-03-24 17:07:26 +0530

    Add files via upload

--- app/views.py
+++ app/views.py
@@ -2,6 +2,7 @@ from app import db
 from app import app
 from flask import Flask, render_template , request
 from  app.models import content_date, user_age_groups , content_topics, visitor_data,content_age_groups, user_topics
+from flask import current_app, flash, jsonify, make_response, redirect, request, url_for
 #from .models import db
 
 
@@ -21,34 +22,36 @@ def visitor_page():
     print(type(user_id))
     print(item_id)
 
-    
     admin_role1 = db.session.query(user_age_groups ).filter_by(user_id = user_id).first()
     admin_role2 = db.session.query(content_topics ).filter_by(item_id = item_id).first()
-    print(admin_role1.age_group_id)
-    print(admin_role2.topic_id)
-    print(admin_role2.item_id)
-    print(type(admin_role1.user_id))
+    if admin_role1 and admin_role2:
+        admin_role1 = db.session.query(user_age_groups ).filter_by(user_id = user_id).first()
+        admin_role2 = db.session.query(content_topics ).filter_by(item_id = item_id).first()
+        print(admin_role1.age_group_id)
+        print(admin_role2.topic_id)
+        print(admin_role2.item_id)
+        print(type(admin_role1.user_id))
 
-    if admin_role1.user_id ==int(user_id) and admin_role2.item_id ==int(item_id)  :
-        print ("in count")
-        v = db.session.query(visitor_data).filter_by(user_id = user_id).first()
-        if not v:
-            me = visitor_data(user_id =admin_role1.user_id ,item_id = admin_role2.item_id, visit_count = 1 )
-            db.session.add(me)
-            db.session.commit()
-        else:
-           print(v.visit_count)    
-           me = visitor_data(user_id =admin_role1.user_id ,item_id = admin_role2.item_id,visit_count = 1 )
-           v.visit_count += 1
-           db.session.add(v)
-           db.session.commit()
+        if admin_role1.user_id ==int(user_id) and admin_role2.item_id ==int(item_id)  :
+            print ("in count")
+            v = db.session.query(visitor_data).filter_by(user_id = user_id).first()
+            if not v:
+                me = visitor_data(user_id =admin_role1.user_id ,item_id = admin_role2.item_id, visit_count = 1 )
+                db.session.add(me)
+                db.session.commit()
+            else:
+                print(v.visit_count)    
+                me = visitor_data(user_id =admin_role1.user_id ,item_id = admin_role2.item_id,visit_count = 1 )
+                v.visit_count += 1
+                db.session.add(v)
+                db.session.commit()
+            
+
+        rows = visitor_data.query.all()
+        return render_template("public/list.html",rows = rows) 
     else:
-        print("no such ID exists")      
-
-    rows = visitor_data.query.all()
-    return render_template("public/list.html",rows = rows) 
-
-    
+            return make_response(jsonify({'error': 'No ID matched'}), 404)
+        
 @app.route('/content_ids', methods=["GET","POST"])
 def content_ids():
     return render_template('public/content.html')
